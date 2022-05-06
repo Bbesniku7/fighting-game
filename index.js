e57132a52e7726a50472254bd764019c474ebf60
Bbesniku7 <Bbesniku7>
2022-05-06 23:24:09 +0200

    creating player & enemy

--- index.js
+++ index.js
@@ -8,10 +8,63 @@ canvas.height = 576;
 //This stnd to fill the color and take 4 property the color and the height and  width property
 c.fillRect(0, 0, canvas.width, canvas.height);
 
+// of course we can use object but since is a game and class is gonne be like how a object should look than we need to do a class
+
+const gravity = 0.2;
 class Sprite {
-  constructor(position) {
-    this.position;
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.height = 150;
+  }
+
+  drawPlayer() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, 50, this.height);
+  }
+
+  update() {
+    this.drawPlayer();
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0;
+    } else this.velocity.y += gravity;
   }
 }
 
-const player = new Sprite();
+const player = new Sprite({
+  position: {
+    x: 0,
+    y: 0,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  },
+});
+
+player.drawPlayer();
+
+const enemy = new Sprite({
+  position: {
+    x: 400,
+    y: 100,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  },
+});
+
+enemy.drawPlayer();
+
+// Here we gonna create an infint loop.
+const animate = () => {
+  window.requestAnimationFrame(animate);
+  c.fillStyle = "black";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  player.update();
+  enemy.update();
+};
+animate();
