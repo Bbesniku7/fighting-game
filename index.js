b44fa38f9b120053a5ecf7c1c658cfd1a99f1d7a
Bbesniku7 <Bbesniku7>
2022-05-07 16:34:57 +0200

    move with event listeners

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 
 // of course we can use object but since is a game and class is gonne be like how a object should look than we need to do a class
 
-const gravity = 0.2;
+const gravity = 0.8;
 class Sprite {
   constructor({ position, velocity }) {
     this.position = position;
     this.velocity = velocity;
     this.height = 150;
+    this.lastKeyPressed = "";
   }
 
   drawPlayer() {
@@ -25,6 +26,7 @@ class Sprite {
 
   update() {
     this.drawPlayer();
+    this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
 
     if (this.position.y + this.height + this.velocity.y >= canvas.height) {
@@ -59,6 +61,28 @@ const enemy = new Sprite({
 
 enemy.drawPlayer();
 
+// Here we gonna make the keys on the game:
+const keys = {
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  w: {
+    pressed: false,
+  },
+  ArrowRight: {
+    pressed: false,
+  },
+  ArrowLeft: {
+    pressed: false,
+  },
+  ArrowUp: {
+    pressed: false,
+  },
+};
+
 // Here we gonna create an infint loop.
 const animate = () => {
   window.requestAnimationFrame(animate);
@@ -66,5 +90,81 @@ const animate = () => {
   c.fillRect(0, 0, canvas.width, canvas.height);
   player.update();
   enemy.update();
+
+  // Player Movement
+  player.velocity.x = 0;
+  if (keys.d.pressed && player.lastKeyPressed === "d") {
+    player.velocity.x = 1;
+  } else if (keys.a.pressed && player.lastKeyPressed === "a") {
+    player.velocity.x = -1;
+  }
+
+  // Enemy Movement
+
+  enemy.velocity.x = 0;
+  if (keys.ArrowRight.pressed && enemy.lastKeyPressed === "ArrowRight") {
+    enemy.velocity.x = 1;
+  } else if (keys.ArrowLeft.pressed && enemy.lastKeyPressed === "ArrowLeft") {
+    enemy.velocity.x = -1;
+  }
 };
 animate();
+
+// This ie where we are creating Event Listeneres.
+// With this function we can take every  key that is it pressing by the  user
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = true;
+      player.lastKeyPressed = "d";
+      break;
+    case "a":
+      keys.a.pressed = true;
+      player.lastKeyPressed = "a";
+      break;
+    case "w":
+      player.velocity.y = -20;
+      break;
+    // Enemy Keys
+    case "ArrowRight":
+      keys.ArrowRight.pressed = true;
+      enemy.lastKeyPressed = "ArrowRight";
+      break;
+    case "ArrowLeft":
+      keys.ArrowLeft.pressed = true;
+      enemy.lastKeyPressed = "ArrowLeft";
+      break;
+    case "ArrowUp":
+      enemy.velocity.y = -20;
+      break;
+  }
+  console.log(event.key);
+});
+
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+    case "w":
+      keys.w.pressed = false;
+      lastKeyPressed = "w";
+      break;
+    // Enemy Keys
+    case "ArrowRight":
+      keys.ArrowRight.pressed = false;
+      enemy.lastKeyPressed = "ArrowRight";
+      break;
+    case "ArrowLeft":
+      keys.ArrowLeft.pressed = false;
+      enemy.lastKeyPressed = "ArrowLeft";
+      break;
+    case "ArrowUp":
+      keys.ArrowUp.pressed = false;
+      break;
+  }
+  console.log("upp event =< ", event.key);
+});
